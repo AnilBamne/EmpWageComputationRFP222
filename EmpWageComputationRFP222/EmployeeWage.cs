bf5f418e9c0c125e2aa25518ff6da24bda07d28f
AnilBamne <anilbamne20@gmail.com>
2022-11-09 22:50:35 +0530

    Adding CalculateWageTill100HrsOrMaxDayReachedUC6 Code

--- EmpWageComputationRFP222/EmployeeWage.cs
+++ EmpWageComputationRFP222/EmployeeWage.cs
@@ -14,35 +14,42 @@ namespace EmpWageComputationRFP222
             const int Is_Full_Time = 1;
             const int Is_Part_Time = 2;
             //UC5
-            int WorkingDaysPerMonth = 20;
+            const int MaxWorkingDaysPerMonth = 20;
+            const int MaxWorkingHrsPerMonth = 100;
+            int TotalWorkingHrs = 0;
             int TotalWageOfMonth = 0;
             int empWage = 0;
             int empHrs = 0;
+            int empWorkingDays = 0;
 
-            for(int i = 1; i <= WorkingDaysPerMonth; i++)
+            while(TotalWorkingHrs <= MaxWorkingHrsPerMonth && empWorkingDays< MaxWorkingDaysPerMonth)
             {
+                empWorkingDays++;
                 Random r = new Random();
                 int empCheck = r.Next(0, 3);
                 //UC4
                 switch (empCheck)
                 {
                     case Is_Full_Time:
-                        Console.WriteLine("The employee Is_Full_Time present");
+                        Console.WriteLine("Employee Is_Full_Time present");
                         empHrs = 8;
                         break;
                     case Is_Part_Time:
-                        Console.WriteLine("The employee Is_Part_Time present");
+                        Console.WriteLine("Employee Is_Part_Time present");
                         empHrs = 4;
                         break;
                     default:
-                        Console.WriteLine("The Employee is abscent");
+                        Console.WriteLine("Employee is abscent");
                         empHrs = 0;
                         break;
                 }
                 //UC2
                 empWage = EMP_RATE_PER_HR * empHrs;
                 TotalWageOfMonth+=empWage;
-                Console.WriteLine("Employee Wage is : " + TotalWageOfMonth);
+                TotalWorkingHrs += empHrs;
+                Console.WriteLine("working day is : " + empWorkingDays);
+                Console.WriteLine("Total working hrs : " + TotalWorkingHrs);
+                Console.WriteLine("Employee Wage is : " + TotalWageOfMonth+"\n");
             }
             
             Console.WriteLine("Total Employee Wage of a month is : "+ TotalWageOfMonth);
