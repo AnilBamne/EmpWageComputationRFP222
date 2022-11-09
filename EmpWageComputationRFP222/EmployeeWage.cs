48014bfb45a9140086fae5642a9293cfccdf746d
AnilBamne <anilbamne20@gmail.com>
2022-11-09 22:08:28 +0530

    Adding SolveUsingSwitchCaseStatementUC4 Code

--- EmpWageComputationRFP222/EmployeeWage.cs
+++ EmpWageComputationRFP222/EmployeeWage.cs
@@ -10,27 +10,29 @@ namespace EmpWageComputationRFP222
             Console.WriteLine("Welcome To EmployeeWageComputation");
             //UC1
             const int EMP_RATE_PER_HR = 20;
-            int Is_Full_Time = 1;
-            int Is_Part_Time = 2;
+            //UC3
+            const int Is_Full_Time = 1;
+            const int Is_Part_Time = 2;
             int empWage = 0;
             int empHrs = 0;
 
             Random r = new Random();
             int empCheck = r.Next(0, 3);
-            if (empCheck == Is_Full_Time)
+            //UC4
+            switch (empCheck)
             {
-                Console.WriteLine("The employee Is_Full_Time present");
-                empHrs = 8;
-            }
-            else if(empCheck == Is_Part_Time)
-            {
-                Console.WriteLine("The employee Is_Part_Time present");
-                empHrs = 4;
-            }
-            else
-            {
-                Console.WriteLine("The Employee is abscent");
-                empHrs = 0;
+                case Is_Full_Time:
+                    Console.WriteLine("The employee Is_Full_Time present");
+                    empHrs = 8;
+                    break;
+                case Is_Part_Time:
+                    Console.WriteLine("The employee Is_Part_Time present");
+                    empHrs = 4;
+                    break;
+                default:
+                    Console.WriteLine("The Employee is abscent");
+                    empHrs = 0;
+                    break;
             }
             //UC2
             empWage = EMP_RATE_PER_HR * empHrs;
