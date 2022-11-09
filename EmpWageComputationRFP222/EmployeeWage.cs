5d915fa9f128347b425bb11613ad51f7292c9c69
AnilBamne <anilbamne20@gmail.com>
2022-11-09 17:10:09 +0530

    Adding EmployeeAttendanceUC1 code

--- EmpWageComputationRFP222/EmployeeWage.cs
+++ EmpWageComputationRFP222/EmployeeWage.cs
@@ -6,7 +6,21 @@ namespace EmpWageComputationRFP222
     {
         static void Main(string[] args)
         {
+            //main
             Console.WriteLine("Welcome To EmployeeWageComputation");
+            //UC1
+            int Is_FullTime = 1;
+            Random r = new Random();
+            int empCheck = r.Next(0, 2);
+
+            if (empCheck == Is_FullTime)
+            {
+                Console.WriteLine("The employee is present");
+            }
+            else
+            {
+                Console.WriteLine("The Employee is abscent");
+            }
         }
     }
 }
