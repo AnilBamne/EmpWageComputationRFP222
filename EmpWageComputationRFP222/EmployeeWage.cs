607ff7cc7d986e66ce5a62874b74f1407a0de025
AnilBamne <anilbamne20@gmail.com>
2022-11-09 22:20:53 +0530

    Adding CalculateWagesForAMonthUC5 Code

--- EmpWageComputationRFP222/EmployeeWage.cs
+++ EmpWageComputationRFP222/EmployeeWage.cs
@@ -13,30 +13,39 @@ namespace EmpWageComputationRFP222
             //UC3
             const int Is_Full_Time = 1;
             const int Is_Part_Time = 2;
+            //UC5
+            int WorkingDaysPerMonth = 20;
+            int TotalWageOfMonth = 0;
             int empWage = 0;
             int empHrs = 0;
 
-            Random r = new Random();
-            int empCheck = r.Next(0, 3);
-            //UC4
-            switch (empCheck)
+            for(int i = 1; i <= WorkingDaysPerMonth; i++)
             {
-                case Is_Full_Time:
-                    Console.WriteLine("The employee Is_Full_Time present");
-                    empHrs = 8;
-                    break;
-                case Is_Part_Time:
-                    Console.WriteLine("The employee Is_Part_Time present");
-                    empHrs = 4;
-                    break;
-                default:
-                    Console.WriteLine("The Employee is abscent");
-                    empHrs = 0;
-                    break;
+                Random r = new Random();
+                int empCheck = r.Next(0, 3);
+                //UC4
+                switch (empCheck)
+                {
+                    case Is_Full_Time:
+                        Console.WriteLine("The employee Is_Full_Time present");
+                        empHrs = 8;
+                        break;
+                    case Is_Part_Time:
+                        Console.WriteLine("The employee Is_Part_Time present");
+                        empHrs = 4;
+                        break;
+                    default:
+                        Console.WriteLine("The Employee is abscent");
+                        empHrs = 0;
+                        break;
+                }
+                //UC2
+                empWage = EMP_RATE_PER_HR * empHrs;
+                TotalWageOfMonth+=empWage;
+                Console.WriteLine("Employee Wage is : " + TotalWageOfMonth);
             }
-            //UC2
-            empWage = EMP_RATE_PER_HR * empHrs;
-            Console.WriteLine("Daily Employee Wage is : "+empWage);
+            
+            Console.WriteLine("Total Employee Wage of a month is : "+ TotalWageOfMonth);
 
         }
     }
