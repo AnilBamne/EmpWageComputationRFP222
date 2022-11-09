5962ad22f7611b24f6bb0b1c7d9efe87185992df
AnilBamne <anilbamne20@gmail.com>
2022-11-09 16:59:24 +0530

    Adding Wwelcome Message into Main Branch

---
+++ EmpWageComputationRFP222/EmployeeWage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmpWageComputationRFP222
+{
+    public class EmployeeWage
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To EmployeeWageComputation");
+        }
+    }
+}
